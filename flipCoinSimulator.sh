05f3e826db38628e6c0548a803b154b83cf257aa
sonvin2916 <sonalchougule07@gmail.com>
2020-06-12 10:17:07 +0530

    UC-2 : Added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
-
-flip=$(($RANDOM%3))
-if [ $flip -eq 1 ]
-then
-        echo " tails"
-else
-        echo "heads"
-fi
-
+head=0
+tail=0
+num=10
+for (( i=0; i<=num; i++ ))
+do
+	flip=$(($RANDOM%3))
+	if [ $flip -eq 1 ]
+	then
+        	((head++))
+	else
+        	((tail++))
+	fi
+done
+echo "Times head was flipped:" $head
+echo "Times tail was flipped:" $tail
